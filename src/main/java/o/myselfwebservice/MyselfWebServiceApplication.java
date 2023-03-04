78cd80edfc85255448facc1e8014ec9573bdf4d6
Jun <ododok@gmail.com>
2023-03-04 01:02:37 +0100

    Initial commit

---
+++ src/main/java/o/myselfwebservice/MyselfWebServiceApplication.java
@@ -0,0 +1,13 @@
+package o.myselfwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyselfWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyselfWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/o/myselfwebservice/MyselfWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package o.myselfwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyselfWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
